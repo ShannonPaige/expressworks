c8975fbafedf83fc65820326ad5cc3b2773247b1
ShannonPaige <spaige@gmail.com>
2016-03-17 23:04:48 -0600

    Add exercises for expressworks

---
+++ params.js
@@ -0,0 +1,14 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.put('/message/:id', function(req, res){
+  var id = req.params.id
+  var string = require('crypto')
+              .createHash('sha1')
+              .update(new Date().toDateString() + id)
+              .digest('hex')
+  res.send(string)
+})
+
+app.listen(process.argv[2] || 3000)
